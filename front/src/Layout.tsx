b4bb8aa8ff1470a30bda69529358f79fef0a6e36
Romain Thibaud <romainthibaud45@gmail.com>
2023-06-28 11:41:46 +0200

    fix(Sidebar): move Sidebar to search

--- front/src/Layout.tsx
+++ front/src/Layout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Nav from "./layout/Nav";
-import Sidebar from "./layout/Sidebar";
 
 function App() {
   return (
     <main className="container grid grid-cols-3 no-scrollbar">
       <Nav />
-      <Sidebar />
       <Outlet />
     </main>
   );
--- front/src/pages/search.tsx
+++ front/src/pages/search.tsx
@@ -1,15 +1,23 @@
 import Card from "../components/Card";
+import Sidebar from "../layout/Sidebar";
 import { userProps } from "../utils/types";
 
 export default function search({ users }: { users: userProps[] }) {
   return (
-    <div className="col-span-2 px-4">
-      <input type="search" className="" placeholder="Rechercher un profil..." />
-      <div className="h-96 overflow-auto px-12 -mt-3">
-        {users.map((user) => (
-          <Card user={user} key={user.firstName + user.lastName} />
-        ))}
+    <>
+      <Sidebar />
+      <div className="col-span-2 px-4">
+        <input
+          type="search"
+          className=""
+          placeholder="Rechercher un profil..."
+        />
+        <div className="h-96 overflow-auto px-12 -mt-3">
+          {users.map((user) => (
+            <Card user={user} key={user.firstName + user.lastName} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
