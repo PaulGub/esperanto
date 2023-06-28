b5c967bf93b95ea345c5254a5bf032a2e4361da5
Samuel R <the.sam.r@hotmail.fr>
2023-06-28 15:52:09 +0200

    create getUserById and getUserByTag (matching function)

--- back/src/controllers/UserControllers.ts
+++ back/src/controllers/UserControllers.ts
@@ -1,15 +1,49 @@
-import {HealthActor, Industrial, Researcher, User} from "@models/index"
+import {HealthActor, Industrial, Researcher, User, Tag} from "@models/index"
+import { Op } from "sequelize";
 
 export const getAllUsers = async () => {
     return User.findAll({
         include: [Industrial, HealthActor, Researcher]
     });
 }
+
 export const getUsersByTagUser = async (userId: number) => {
-    const users =  User.findAll({
-        include: [Industrial, HealthActor, Researcher]
+    const currentUser = await User.findByPk(userId, {
+        include: [Tag]
     });
-    console.log(users)
-    console.log(userId);
-    return users;
+
+    const currentUserTags = currentUser.dataValues.tags.map((tag: any) => tag.name);
+
+    const users = await User.findAll({
+        where: {
+            id: {
+                [Op.ne]: userId
+            }
+        },
+        include: [Tag, Industrial, HealthActor, Researcher]
+    });
+
+    const usersWithCommonTags = users.map((user: any) => {
+        const userTags = user.tags.map((tag: any) => tag.name);
+        const commonTags = currentUserTags.filter((tag: string) => userTags.includes(tag));
+        const commonTagCount = commonTags.length;
+        return {
+            user,
+            commonTagCount
+        };
+    });
+
+    usersWithCommonTags.sort((a: any, b: any) => b.commonTagCount - a.commonTagCount);
+
+    const sortedUsers = usersWithCommonTags.map((item: any) => item.user);
+
+    return sortedUsers;
+}
+
+export const getUserById = async (userId: number) => {
+    const user = await User.findByPk(userId, {
+        include: [Tag, Industrial, HealthActor, Researcher]
+    });
+    console.log(user)
+    return user;
 }
--- back/src/graphql/User/resolvers.ts
+++ back/src/graphql/User/resolvers.ts
@@ -1,8 +1,9 @@
-import { getAllUsers, getUsersByTagUser } from '@controllers/UserControllers';
+import { getAllUsers, getUsersByTagUser, getUserById } from '@controllers/UserControllers';
 
 export default {
   Query: {
     users: () => getAllUsers(),
     usersByTagUser: async (_, args: { userId: number }) => getUsersByTagUser(args.userId),
+    userById: async (_, args: { userId: number }) => getUserById(args.userId),
   },
 };
