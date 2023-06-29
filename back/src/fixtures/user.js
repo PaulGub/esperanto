6a275ac38cd309fb9e236c4e0d97284b7bc763ea
tjamard <jamard.theo17@gmail.com>
2023-06-29 11:03:20 +0200

    [backend] fix: don't duplicate data

--- back/src/fixtures/user.js
+++ back/src/fixtures/user.js
@@ -2,7 +2,7 @@ import { faker } from '@faker-js/faker/locale/fr';
 import { User, HealthActor, Researcher, Industrial, Tag } from '@models/index';
 import CONST from '@server/CONST';
 
-const createFakeUser = async (count, role, modelIncluded, additionalFields) => {
+export const createFakeUsersHealthActor = async (count) => {
   for (let i = 0; i < count; i++) {
     const tags = faker.helpers.arrayElements(CONST.HEALTH_KEY_WORDS, { min: 3, max: 30 });
 
@@ -15,8 +15,11 @@ const createFakeUser = async (count, role, modelIncluded, additionalFields) => {
       healthNetwork: faker.helpers.arrayElement(CONST.HEALTH_NETWORKS),
       professionalStatus: faker.lorem.paragraph(),
       experiences: faker.lorem.paragraph(),
-      role: role,
-      ...additionalFields,
+      role: CONST.ROLES.HEALTH_ACTOR,
+      healthActor: {
+        careServiceType: faker.helpers.arrayElement(CONST.CARE_SERVICES),
+        supportServices: faker.helpers.arrayElement(CONST.SUPPORT_SERVICES),
+      },
     };
 
     const existingUser = await User.findOne({ where: { email: fakeUser.email } });
@@ -26,42 +29,87 @@ const createFakeUser = async (count, role, modelIncluded, additionalFields) => {
       continue;
     }
 
-    const user = await User.create(fakeUser, { include: [modelIncluded] });
+    const user = await User.create(fakeUser, { include: [HealthActor] });
 
     for (const tagName of tags) {
       const [tag] = await Tag.findOrCreate({ where: { name: tagName } });
       await user.addTag(tag);
     }
   }
+  console.log(`✅  ${count} acteurs de la santé créés`);
 }
 
-export const createFakeUsersHealthActor = async (count) => {
-  await createFakeUser(count, CONST.ROLES.HEALTH_ACTOR, HealthActor, {
-    healthActor: {
-      careServiceType: faker.helpers.arrayElement(CONST.CARE_SERVICES),
-      supportServices: faker.helpers.arrayElement(CONST.SUPPORT_SERVICES),
-    },
-  });
-  console.log(`✅  ${count} acteurs de la santé créés`)
-};
-
 export const createFakeUsersResearcher = async (count) => {
-  await createFakeUser(count, CONST.ROLES.RESEARCHER, Researcher, {
-    researcher: {
-      researchUnitName: faker.helpers.arrayElement(CONST.RESEARCH_UNITS),
-      researchDepartment: faker.helpers.arrayElement(CONST.RESEARCH_DEPARTMENTS),
-      researchArea: faker.helpers.arrayElement(CONST.RESEARCH_AREAS),
-      description: faker.lorem.paragraph(),
-    },
-  });
-  console.log(`✅  ${count} chercheurs créés`)
-};
+  for (let i = 0; i < count; i++) {
+    const tags = faker.helpers.arrayElements(CONST.HEALTH_KEY_WORDS, { min: 3, max: 30 });
+
+    const fakeUser = {
+      firstname: faker.person.firstName(),
+      lastname: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      phoneNumber: faker.phone.number('+33 6 ## ## ## ##'),
+      healthNetwork: faker.helpers.arrayElement(CONST.HEALTH_NETWORKS),
+      professionalStatus: faker.lorem.paragraph(),
+      experiences: faker.lorem.paragraph(),
+      role: CONST.ROLES.HEALTH_ACTOR,
+      researcher: {
+        researchUnitName: faker.helpers.arrayElement(CONST.RESEARCH_UNITS),
+        researchDepartment: faker.helpers.arrayElement(CONST.RESEARCH_DEPARTMENTS),
+        researchArea: faker.helpers.arrayElement(CONST.RESEARCH_AREAS),
+        description: faker.lorem.paragraph(),
+      },
+    };
+
+    const existingUser = await User.findOne({ where: { email: fakeUser.email } });
+
+    if (existingUser) {
+      console.log(`User with email ${fakeUser.email} already exists. Skipping...`);
+      continue;
+    }
+
+    const user = await User.create(fakeUser, { include: [Researcher] });
+
+    for (const tagName of tags) {
+      const [tag] = await Tag.findOrCreate({ where: { name: tagName } });
+      await user.addTag(tag);
+    }
+  }
+  console.log(`✅  ${count} chercheurs créés`);
+}
 
 export const createFakeUsersIndustrial = async (count) => {
-  await createFakeUser(count, CONST.ROLES.INDUSTRIAL, Industrial, {
-    industrial: {
-      careSector: faker.helpers.arrayElement(CONST.CARE_SECTORS),
-    },
-  });
-  console.log(`✅  ${count} industriels créés`)
-};
+  for (let i = 0; i < count; i++) {
+    const tags = faker.helpers.arrayElements(CONST.HEALTH_KEY_WORDS, { min: 3, max: 30 });
+
+    const fakeUser = {
+      firstname: faker.person.firstName(),
+      lastname: faker.person.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      phoneNumber: faker.phone.number('+33 6 ## ## ## ##'),
+      healthNetwork: faker.helpers.arrayElement(CONST.HEALTH_NETWORKS),
+      professionalStatus: faker.lorem.paragraph(),
+      experiences: faker.lorem.paragraph(),
+      role: CONST.ROLES.HEALTH_ACTOR,
+      industrial: {
+        careSector: faker.helpers.arrayElement(CONST.CARE_SECTORS),
+      }
+    };
+
+    const existingUser = await User.findOne({ where: { email: fakeUser.email } });
+
+    if (existingUser) {
+      console.log(`User with email ${fakeUser.email} already exists. Skipping...`);
+      continue;
+    }
+
+    const user = await User.create(fakeUser, { include: [Industrial] });
+
+    for (const tagName of tags) {
+      const [tag] = await Tag.findOrCreate({ where: { name: tagName } });
+      await user.addTag(tag);
+    }
+  }
+  console.log(`✅  ${count} industriels créés`);
+}
