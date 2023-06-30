36d6546b2536bbf79b9d61b3934252bce548280b
Romain Thibaud <romainthibaud45@gmail.com>
2023-06-30 14:31:51 +0200

    fix(data.d.ts): user type from back

--- front/src/utils/types/data.d.ts
+++ front/src/utils/types/data.d.ts
@@ -12,6 +12,39 @@ export type userProps = {
   description?: string;
   profilePicture?: string;
   profileBanner?: string;
+  tags: Array<{ id: number; name: string }>;
+};
+
+export type globalUserProps = {
+  description: string;
+  email: string;
+  experiences: string;
+  firstname: string;
+  healthActor: {
+    careServiceType: string;
+    id: string;
+    supportServices: string;
+  };
+  healthNetwork: string;
+  id: string;
+  industrial: {
+    id: string;
+    careSector: string;
+    otherSector: string;
+  };
+  lastname: string;
+  phoneNumber: string;
+  professionalStatus: string;
+  profileBanner: string;
+  profilePicture: string;
+  researcher: {
+    id: string;
+    otherSector: string;
+    researchArea: string;
+    researchUnitName: string;
+  };
+  role: string;
+  tags: string;
 };
 
 export type needProps = {
