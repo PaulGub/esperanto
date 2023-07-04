cf97e057f44dc8bb773a51ead93814127a4bcc37
tjamard <jamard.theo17@gmail.com>
2023-07-04 12:05:04 +0200

    [backend] feat: add need creation

--- back/src/graphql/Need/resolvers.ts
+++ back/src/graphql/Need/resolvers.ts
@@ -1,9 +1,14 @@
-import { getAllNeeds, getNeedsById, getNeedByUserIdSuggestion } from '@controllers/NeedControllers';
+import {createNeed, getAllNeeds, getNeedByUserIdSuggestion, getNeedsById} from '@controllers/NeedControllers';
+import {NeedT} from "@models/Need";
+import {NeedData} from "@server/types";
 
 export default {
     Query: {
         needs: () => getAllNeeds(),
         needByNeedId: async (_, args: { needsId: number }) => getNeedsById(args.needsId),
         needByUserIdSuggestion: async (_, args: { userId: number }) => getNeedByUserIdSuggestion(args.userId)
+    },
+    Mutation: {
+        createNeed: async (_, args: { userId: number, needData: NeedData }): Promise<NeedT> => createNeed(args),
     },
 };
