490d9abb0fa3f2292bd61463df26de068f8f107c
tjamard <jamard.theo17@gmail.com>
2023-07-04 16:10:47 +0200

    [backend] feat: followers and check if followed

--- back/src/graphql/User/resolvers.ts
+++ back/src/graphql/User/resolvers.ts
@@ -9,11 +9,12 @@ import {
   createResearcher,
   getAllFollowers,
   addFollow,
-  removeFollow
+  removeFollow,
+  getAllFollowing,
+  checkIsFollowed
 } from '@controllers/UserControllers';
 
 import { HealthActorTypes, UserInterface } from "@server/types";
-
 export default {
   Query: {
     users: async (): Promise<UserInterface[]> => getAllUsers(),
@@ -21,6 +22,8 @@ export default {
     userById: async (_, args: { userId: number }): Promise<UserInterface> => getUserById(args.userId),
     usersByTagNeed: async (_, args: { userId: number, needId: number }) => getUsersByTagNeed(args.userId, args.needId),
     getAllFollowers: async (_, args: { userId: number }) => getAllFollowers(args.userId),
+    getAllFollowing: async (_, args: { userId: number }) => getAllFollowing(args.userId),
+    checkIsFollowed: async (_, args: { userId: number, followerId: number }): Promise<Boolean> => checkIsFollowed(args)
   },
   Mutation: {
     createUser: async (_, args) => {
--- back/src/models/associations.ts
+++ back/src/models/associations.ts
@@ -71,6 +71,6 @@ export const associations = (): void => {
     ListMaterial.belongsToMany(User, { through: 'ListMaterial_User' });
     User.belongsToMany(ListMaterial, { through: 'ListMaterial_User' });
 
-    User.belongsToMany(User, { as: 'Followers', through: 'follower', foreignKey: 'userId', otherKey: 'followerId' });
-    User.belongsToMany(User, { as: 'Following', through: 'follower', foreignKey: 'followerId', otherKey: 'userId' });
+    User.belongsToMany(User, { as: 'Following', through: 'follower', foreignKey: 'userId', otherKey: 'followerId' });
+    User.belongsToMany(User, { as: 'Followers', through: 'follower', foreignKey: 'followerId', otherKey: 'userId' });
 };
