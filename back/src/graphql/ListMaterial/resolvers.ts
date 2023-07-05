379ec18c6e12724d40327c3809997b58e6a69deb
tjamard <jamard.theo17@gmail.com>
2023-07-05 14:03:09 +0200

    [backend] feat: listMaterial mutations

--- back/src/graphql/ListMaterial/resolvers.ts
+++ back/src/graphql/ListMaterial/resolvers.ts
@@ -1,8 +1,12 @@
 import {ListMaterialsT} from "@server/models";
-import { getListMaterialByUserId} from "@controllers/ListMaterialControllers";
+import {createListMaterial, getListMaterialByUserId, addMaterialToList} from "@controllers/ListMaterialControllers";
 
 export default {
     Query: {
         listMaterialByUserId: async (_, args: { userId: number }): Promise<ListMaterialsT[]> => getListMaterialByUserId(args.userId),
-    }
+    },
+    Mutation: {
+        createListMaterial: async (_, args: { userId: number, listName: string, arrayMaterialId: number[] }): Promise<ListMaterialsT> => createListMaterial(args),
+        addMaterialToList: async (_, args: { userId: number, listId: number, arrayMaterialId: number[] }): Promise<ListMaterialsT|Error> => addMaterialToList(args),
+    },
 };
